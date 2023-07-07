88815737ecd1d1e56c3c30dbe295bb1a40314619
Mahsa Radinmehr <rodinmehr@gmail.com>
2023-07-07 21:01:34 +0330

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by <a href="https://thunderous-maamoul-e91194.netlify.app/" target="_blank" rel="noopener noreferrer">Mahsa Radinmehr</a> and is <a href="https://rodinmehr.github.io/" target="_blank" rel="noopener noreferrer">open-sourced on GitHub</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by <a href="https://thunderous-maamoul-e91194.netlify.app/" target="_blank" rel="noopener noreferrer">Mahsa Radinmehr</a> and is <a href="https://rodinmehr.github.io/" target="_blank" rel="noopener noreferrer">open-sourced on GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
