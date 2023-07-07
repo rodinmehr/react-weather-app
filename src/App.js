3f8d8fc6584c9d258eaf9389f7c616a04cea85df
Mahsa Radinmehr <rodinmehr@gmail.com>
2023-07-07 22:59:25 +0330

    add basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project is coded by <a href="https://thunderous-maamoul-e91194.netlify.app/" target="_blank" rel="noopener noreferrer">Mahsa Radinmehr</a> and is <a href="https://rodinmehr.github.io/" target="_blank" rel="noopener noreferrer">open-sourced on GitHub</a>
         </footer>
